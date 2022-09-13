1f34003111ea1b4e64b501990cd6075374f5f483
fermi-paradox <86816389+the-fermi-paradox@users.noreply.github.com>
2022-09-13 15:53:18 -0500

    Major update to move to only unsigned int for everything.
    We want to avoid narrow types if at all possible. Narrow types in
    arithmetic lead to implicit conversion, which leads to all sorts of
    subtle bugs.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS = -std=c18 -Wall -Wextra -O0 -g3 -I.
+CFLAGS = -std=c18 -Wall -Wconversion -Wsign-conversion -Wextra -O0 -g3 -I.
 main : opcodes.h
 
 .PHONY : clean
--- main.c
+++ main.c
@@ -1,238 +1,389 @@
 #include <inttypes.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <opcodes.h>
+// #define NDEBUG
+#include <assert.h>
 
-static uint8_t memory[(uint16_t) -1] = {0};
+/* main memory */
+static unsigned memory[(unsigned) -1] = {0};
+/* conditional bit flags */
+enum Flags {FLAG_CY = 0, FLAG_P = 2, FLAG_AC = 4, FLAG_Z = 6, FLAG_S = 7};
+static unsigned flags = 0;
 
 /* register array */
+/* stored in order Hi/Lo */
 enum RegCode {rB = 0, rC = 1, rD = 2, rE = 3, rH = 4, rL = 5, rPSW = 6, rA = 7, RG_COUNT};
+static unsigned reg[RG_COUNT] = {0};
+static unsigned *B = reg + rB;
+static unsigned *C = reg + rC;
+static unsigned *D = reg + rD;
+static unsigned *E = reg + rE;
+static unsigned *H = reg + rH;
+static unsigned *L = reg + rL;
+static unsigned *PSW = reg + rPSW;
+static unsigned *A = reg + rA;
+static unsigned *sp = NULL;
 
 /* masks */
+/* position of the most significant bit */
 #define PMSB_8 7U
+/* position of the least significant bit */
 #define PLSB_8 0U
-#define MSB_8 1U << PMSB_8
-#define LSB_8 1U << PLSB_8
+/* most significant bit */
+#define MSB_8 (1U << PMSB_8)
+/* least significant bit */
+#define LSB_8 (1U << PLSB_8)
+/* position of the "overflow" bit in certain operations */
+#define POVERFLOW_8 8U
+/* the overflow bit */
+#define OVERFLOW_8 (1U << POVERFLOW_8)
+/* bitmasks for keeping our bits right */
+#define BITMASK_8  0xFF
+#define BITMASK_16 0xFFFF
 
-/* flags */
-static uint8_t flags = 0;
-#define FLAG_S  7U
-#define FLAG_Z  6U
-#define FLAG_AC 4U
-#define FLAG_P  2U
-#define FLAG_CY 0U
-
-static inline uint8_t emu8080_set_bit(uint8_t bitfield, uint8_t pos, _Bool x) {
-  return ((bitfield & ~(1 << pos)) | (x << pos));
+/* set the bit at pos in bitfield to x, return the modified bitfield */
+static inline unsigned e80_set_bit(unsigned bitfield, unsigned pos, unsigned x) {
+  assert(pos <= PMSB_8 && pos >= 0);
+  assert(bitfield <= UINT8_MAX);
+  return (bitfield & ~(1U << pos)) | (!!x << pos);
 }
 
-static inline uint8_t emu8080_set_carry(_Bool x) {
-  return emu8080_set_bit(flags, FLAG_CY, x);
+/* get the bit at pos in bitfield */
+static inline unsigned e80_get_bit(unsigned bitfield, unsigned pos) {
+  assert(pos <= PMSB_8 && pos >= 0);
+  assert(bitfield <= UINT8_MAX);
+  return !!(bitfield & (1U << pos));
 }
 
-static inline uint8_t emu8080_get_carry() {
-  return flags & (1 << FLAG_CY);
+/* set the bit at pos in flags to x */
+static inline void e80_set_flag(unsigned pos, unsigned x) {
+  flags = e80_set_bit(flags, pos, x);
 }
 
-static inline uint8_t emu8080_right_rotate(uint8_t bitfield) {
-  return (bitfield >> 1) | (bitfield << (sizeof(uint8_t) - 1));
+/* get the bit at pos in flags */
+static inline unsigned e80_get_flag(unsigned pos) {
+  return !!(e80_get_bit(flags, pos));
 }
 
-static inline uint8_t emu8080_left_rotate(uint8_t bitfield) {
-  return (bitfield << 1) | (bitfield >> (sizeof(uint8_t) - 1));
+static inline unsigned e80_check_parity(unsigned value) {
+  unsigned parity = 1;
+  while (value) {
+    parity = !parity;
+    value &= (value - 1U);
+  }
+  return parity;
 }
 
-static inline uint8_t emu8080_lrotate(uint8_t* reg) {
-  *reg = emu8080_left_rotate(*reg);
-  /* set the carry bit to the LSB */
-  emu8080_set_carry(*reg & LSB_8);
-  return *reg;
+static inline unsigned e80_check_zero(unsigned value) {
+  return !value;
 }
 
-static inline uint8_t emu8080_lrotate_c(uint8_t* reg) {
-  uint8_t tmp = (*reg & MSB_8) >> PMSB_8;
-  *reg = emu8080_left_rotate(*reg);
-  emu8080_set_bit(*reg, PLSB_8, emu8080_get_carry());
-  emu8080_set_carry(tmp);
-  return *reg;
+static inline unsigned e80_check_sign(unsigned value) {
+  return !!(value & 0x80U);
 }
 
-static inline uint8_t emu8080_rrotate(uint8_t* reg) {
-  *reg = emu8080_right_rotate(*reg);
-  /* set the carry bit to the MSB */
-  emu8080_set_carry((*reg >> PMSB_8));
-  return *reg;
+static inline unsigned e80_check_carry(unsigned value) {
+  return !!(value & 0x100U);
 }
 
-static inline uint8_t emu8080_rrotate_c(uint8_t* reg) {
-  uint8_t tmp = *reg & LSB_8;
-  *reg = emu8080_right_rotate(*reg);
-  emu8080_set_bit(*reg, PMSB_8, emu8080_get_carry());
-  emu8080_set_carry(tmp);
-  return *reg;
+static inline unsigned e80_check_half_carry(unsigned v1, unsigned v2) {
+  return !!(((v1 & 0x0fU) + (v2 & 0x0fU)) & 0x10U);
 }
 
-static inline uint8_t emu8080_incr_reg(uint8_t* reg) {
+
+static inline void e80_increment(unsigned* p) {
+  assert(p);
+  assert(*p <= UINT8_MAX);
+  unsigned sum = *p + 1;
+  e80_set_flag(FLAG_P,  e80_check_parity(sum));
+  e80_set_flag(FLAG_AC, e80_check_half_carry(*p, 1));
+  e80_set_flag(FLAG_Z,  e80_check_zero(sum));
+  e80_set_flag(FLAG_S,  e80_check_sign(sum));
+  *p = sum & BITMASK_8;
+}
+
+static inline void e80_add(unsigned value) {
+  unsigned carry = e80_get_flag(FLAG_CY);
+  unsigned sum = *A + value + carry;
+
+  /* Handle condition bits */
+
+  e80_set_flag(FLAG_CY, e80_check_carry(sum));
+  e80_set_flag(FLAG_AC, e80_check_half_carry(*A, value));
+  e80_set_flag(FLAG_Z,  e80_check_zero(sum));
+  e80_set_flag(FLAG_P,  e80_check_parity(sum));
+  e80_set_flag(FLAG_S,  e80_check_sign(sum));
+  *A = sum & BITMASK_8;
+}
+
+static inline void e80_subtract(unsigned value) {
+  
+}
+
+/* performs a left rotation with the carry bit set to the LSB's new value */
+static inline unsigned e80_lrotate(unsigned* reg) {
+  assert(reg != NULL);
+  assert(*reg <= UINT8_MAX);
+
+  e80_set_flag(FLAG_CY, !!(*reg & MSB_8));
+  *reg = (*reg << 1U) | (*reg >> (PMSB_8));
+  return *reg = *reg & BITMASK_8;
+}
+
+/* performs a left rotation with the carry flag acting as a 9th bit */
+/* in other words, the rotation is done THROUGH the carry */
+static inline unsigned e80_lrotate_c(unsigned* reg) {
+  assert(reg != NULL);
+  assert(*reg <= UINT8_MAX);
+
+  unsigned tmp, cbit;
+  cbit = !!(*reg & MSB_8);
+  tmp = *reg | (e80_get_flag(FLAG_CY) << POVERFLOW_8);
+  tmp = (tmp << 1) | (tmp >> POVERFLOW_8);
+  e80_set_flag(FLAG_CY, cbit);
+  return *reg = tmp & BITMASK_8;
+}
+
+/* performs a right rotation with the carry bit set to the MSB's new value */
+static inline unsigned e80_rrotate(unsigned* reg) {
+  assert(reg != NULL);
+  assert(*reg <= UINT8_MAX);
+
+  e80_set_flag(FLAG_CY, !!(*reg & LSB_8));
+  *reg = (*reg >> 1U) | (*reg << PMSB_8);
+  return *reg = *reg & BITMASK_8;
+}
+
+/* performs a right rotation with the carry flag acting as a 9th bit */
+/* in other words, the rotation is done THROUGH the carry */
+static inline unsigned e80_rrotate_c(unsigned* reg) {
+  assert(reg != NULL);
+  assert(*reg <= UINT8_MAX);
+
+  unsigned tmp, cbit;
+  cbit = *reg & LSB_8;
+  tmp = *reg | (e80_get_flag(FLAG_CY) << POVERFLOW_8);
+  tmp = (tmp >> 1);
+  return *reg = tmp & BITMASK_8;
+}
+
+/* increments a register and returns the incremented register's value */
+static inline unsigned e80_incr_reg(unsigned* reg) {
+  assert(reg != NULL);
+  assert(*reg >= 0U && *reg <= UINT8_MAX);
   return ++(*reg);
 }
 
-static inline uint8_t emu8080_decr_reg(uint8_t* reg) {
+/* decrements a register and returns the decremented register's value */
+static inline unsigned e80_decr_reg(unsigned* reg) {
+  assert(reg != NULL);
+  assert(*reg >= 0U && *reg <= UINT8_MAX);
   return --(*reg);
 }
 
 /* assigns val to register reg */
-static inline uint8_t emu8080_assn_reg(uint8_t* reg, uint8_t val) {
+static inline unsigned e80_assn_reg(unsigned* reg, unsigned val) {
+  assert(reg != NULL);
   return *reg = val;
 }
 
 /* returns the hi part; sets the value of the lo part */
-static inline uint8_t emu8080_split16(uint16_t num, uint8_t* lo) {
-  uint16_t hi = (0xf0 & num) >> 8;
-  *lo = (uint8_t) (0x0f & num);
-  return (uint8_t) hi;
+static inline unsigned e80_split16(unsigned num, unsigned* lo_reg) {
+  unsigned hi_val = (0xf0 & num) >> 8;
+  *lo_reg = 0x0f & num;
+  return hi_val;
+}
+
+/* merge two bytes (hibyte, lobyte) to form a 2-byte (16-bit) integer */
+static inline unsigned e80_merge16(unsigned hibyte, unsigned lobyte) {
+  return (hibyte << 8) | (lobyte);
 }
 
 /* takes in the hi part of a register pair and returns the result
    of merging it with its lo part */
-static inline uint16_t emu8080_merge_rp(uint8_t* hi) {
-  return ((uint16_t) (*hi) << 8) | *(hi + 1);
+static inline unsigned e80_merge_rp(unsigned* hi_reg) {
+  return e80_merge16(*hi_reg, *(hi_reg + 1));
 }
 
 /* assigns two bytes (b1, b2) to the register pair pointed to by hi */
-static inline uint8_t emu8080_assn_rp(uint8_t* hi, uint8_t b1, uint8_t b2) {
-  *hi = b1;
-  *(hi + 1) = b2;
-  return *hi;
+static inline unsigned e80_assn_rp(unsigned* hi_reg, unsigned hibyte, unsigned lobyte) {
+  *hi_reg = hibyte;
+  *(hi_reg + 1) = lobyte;
+  return *hi_reg;
 }
 
 /* increments each part of a register pair by one */
-static inline uint8_t emu8080_incr_rp(uint8_t* hi) {
-  uint16_t rp = emu8080_merge_rp(hi);
-  *hi = emu8080_split16(rp + 1, hi + 1);
+static inline unsigned e80_incr_rp(unsigned* hi) {
+  unsigned rp = e80_merge_rp(hi);
+  *hi = e80_split16(rp + 1, hi + 1);
   return *hi;
 }
 
 /* decrements each part of a register pair by one */
-static inline uint8_t emu8080_decr_rp(uint8_t* hi) {
-  uint16_t rp = emu8080_merge_rp(hi);
-  *hi = emu8080_split16(rp - 1, hi + 1);
-  return *hi;
+static inline unsigned e80_decr_rp(unsigned* hi_reg) {
+  unsigned rp = e80_merge_rp(hi_reg);
+  *hi_reg = e80_split16(rp - 1, hi_reg + 1);
+  return *hi_reg;
 }
 
 /* adds two register pairs, storing the result in dest (hi and lo)
  * src and dest should be the hi parts of their pairs */
-static inline uint16_t emu8080_add_rp(uint8_t* src, uint8_t* dest) {
-  uint16_t r1 = emu8080_merge_rp(src);
-  uint16_t r2 = emu8080_merge_rp(dest);
-  uint16_t sum = r1 + r2;
+static inline unsigned e80_add_rp(unsigned* src, unsigned* dest) {
+  unsigned r1 = e80_merge_rp(src);
+  unsigned r2 = e80_merge_rp(dest);
+  unsigned sum = r1 + r2;
   /* there's a carry */
   if (sum < r1)
     flags |= (1 << FLAG_CY);
-  *dest = emu8080_split16(sum, (dest + 1));
+  *dest = e80_split16(sum, (dest + 1));
   return sum;
 }
 
-static inline uint8_t emu8080_read_mem(uint16_t address) {
+static inline unsigned e80_read_mem(uint16_t address) {
   return memory[address];
 }
 
-static inline uint8_t emu8080_write_mem(uint16_t address, uint8_t val) {
+static inline unsigned e80_write_mem(uint16_t address, unsigned val) {
   return memory[address] = val;
 }
 
-static inline uint8_t emu8080_write_memrp(uint8_t* hi, uint8_t val) {
-  return emu8080_write_mem(emu8080_merge_rp(hi), val);
+static inline unsigned e80_write_mem_rpaddr(unsigned* hi_reg, unsigned val) {
+  return e80_write_mem(e80_merge_rp(hi_reg), val);
 }
 
-static inline uint8_t emu8080_read_memrp(uint8_t* hi) {
-  return emu8080_read_mem(emu8080_merge_rp(hi));  
+/* using the register pair as data, write to the address */
+static inline unsigned e80_write_mem_rpdata(unsigned* hi_reg, unsigned hibyte, unsigned lobyte) {
+  unsigned addr = e80_merge16(hibyte, lobyte);
+  e80_write_mem(addr, *(hi_reg + 1));
+  e80_write_mem(addr + 1, *hi_reg);
+  return *hi_reg;
 }
 
-int main(int argc, char** argv)
+static inline unsigned e80_read_mem_rpaddr(unsigned* hi_reg) {
+  return e80_read_mem(e80_merge_rp(hi_reg));
+}
+
+int main(void)
 {
-  /* stored in order Hi/Lo */
-  uint8_t reg[RG_COUNT] = {
-    [rB]   = 0,
-    [rC]   = 0,
-    [rD]   = 0,
-    [rE]   = 0,
-    [rH]   = 0,
-    [rL]   = 0,
-    [rPSW] = 0,
-    [rA]   = 0,
-  };
-  uint8_t* B = reg + rB;
-  uint8_t* C = reg + rC;
-  uint8_t* D = reg + rD;
-  uint8_t* E = reg + rE;
-  uint8_t* H = reg + rH;
-  uint8_t* L = reg + rL;
-  uint8_t* PSW = reg + rPSW;
-  uint8_t* A = reg + rA;
-  uint8_t* sp = NULL;
   /* TODO: Load program into memory */
   /* TODO: Initialize program counter to first instruction of program */
+
   /* Processor Cycle */
 
   /* pc (program counter) as a 16-bit integer instead of a pointer has some nice features
    * memory[pc] can never access bad memory. If incremented past (1 << 16) - 1,
-   * it will just loop around to 0, which guarantees it will be in range */
+   * it will just loop around to 0, which guarantees it will be in range.
+   */
   for (uint16_t pc = 0;;pc++) {
-    enum OpCode opcode = emu8080_read_mem(pc);
+    enum OpCode opcode = e80_read_mem(pc);
     /* These operations are always safe */
-    uint8_t data1 = emu8080_read_mem(pc);
-    uint8_t data2 = emu8080_read_mem(pc + 1);
+    unsigned lo_data = e80_read_mem(pc);
+    unsigned hi_data = e80_read_mem(pc + 1);
     /* TODO: Op Code switch statement */
     switch (opcode) {
-    case NOP:    break;
-    case LXI_B:  emu8080_assn_rp(B, data2, data1); pc += 2; break;
-    case STAX_B: emu8080_write_memrp(B, *A); break;
-    case INX_B:  emu8080_incr_rp(B); break;
-    case INR_B:  emu8080_incr_reg(B); break;
-    case DCR_B:  emu8080_decr_reg(B); break;
-    case MVI_B:  emu8080_assn_reg(B, data1); pc++; break;
-    case RLC:    emu8080_lrotate(A); break;
-    case DSUB:   break; /* Not implemented in 8080 */
-    case DAD_B:  emu8080_add_rp(B, H); break;
-    case LDAX_B: emu8080_assn_reg(A, emu8080_read_memrp(B)); break;
-    case DCX_B:  emu8080_decr_rp(B); break;
-    case INR_C:  emu8080_incr_reg(C); break;
-    case DCR_C:  emu8080_decr_reg(C); break;
-    case MVI_C:  emu8080_assn_reg(C, data1); pc++; break;
-    case RRC:    emu8080_rrotate(A); break;
-	
-    case AHRL:   break; /* Not implemented in 8080 */
-    case LXI_D:  emu8080_assn_rp(D, data2, data1); pc += 2; break;
-    case STAX_D: emu8080_write_memrp(D, *A); break;
-    case INX_D:  emu8080_incr_rp(D); break;
-    case INR_D:  emu8080_incr_reg(D); break;
-    case DCR_D:  emu8080_decr_reg(D); break;
-    case MVI_D:  emu8080_assn_reg(D, data1); pc++; break;
-    case RAL:    emu8080_lrotate_c(A); break;
+    case NOP:
+      break;
+    case LXI_B:
+      e80_assn_rp(B, hi_data, lo_data);
+      pc++; pc++;
+      break;
+    case STAX_B:
+      e80_write_mem_rpaddr(B, *A);
+      break;
+    case INX_B:
+      e80_incr_rp(B);
+      break;
+    case INR_B:
+      e80_incr_reg(B);
+      break;
+    case DCR_B:
+      e80_decr_reg(B);
+      break;
+    case MVI_B:
+      e80_assn_reg(B, lo_data);
+      pc++;
+      break;
+    case RLC:
+      e80_lrotate(A);
+      break;
+    case DSUB:
+      break; /* Not implemented in 8080 */
+    case DAD_B:
+      e80_add_rp(B, H);
+      break;
+    case LDAX_B:
+      e80_assn_reg(A, e80_read_mem_rpaddr(B));
+      break;
+    case DCX_B:
+      e80_decr_rp(B);
+      break;
+    case INR_C:
+      e80_incr_reg(C);
+      break;
+    case DCR_C:
+      e80_decr_reg(C);
+      break;
+    case MVI_C:
+      e80_assn_reg(C, lo_data);
+      pc++;
+      break;
+    case RRC:
+      e80_rrotate(A);
+      break;
+    case AHRL:
+      break; /* Not implemented in 8080 */
+    case LXI_D:
+      e80_assn_rp(D, hi_data, lo_data);
+      pc++;
+      pc++;
+      break;
+    case STAX_D:
+      e80_write_mem_rpaddr(D, *A);
+      break;
+    case INX_D:
+      e80_incr_rp(D);
+      break;
+    case INR_D:
+      e80_incr_reg(D);
+      break;
+    case DCR_D:
+      e80_decr_reg(D);
+      break;
+    case MVI_D:
+      e80_assn_reg(D, lo_data);
+      pc++;
+      break;
+    case RAL:    e80_lrotate_c(A); break;
     case RDEL:   break; /* Not implemented in 8080 */
-    case DAD_D:  emu8080_add_rp(D, H); break;
-    case LDAX_D: emu8080_assn_reg(A, emu8080_read_memrp(D)); break;
-    case DCX_D:  emu8080_decr_rp(D); break;
-    case INR_E:  emu8080_incr_reg(E); break;
-    case DCR_E:  emu8080_decr_reg(E); break;
-    case MVI_E:  emu8080_assn_reg(E, data1); pc++; break;
-    case RAR:    emu8080_rrotate_c(A); break;
+    case DAD_D:  e80_add_rp(D, H); break;
+    case LDAX_D: e80_assn_reg(A, e80_read_mem_rpaddr(D)); break;
+    case DCX_D:  e80_decr_rp(D); break;
+    case INR_E:  e80_incr_reg(E); break;
+    case DCR_E:  e80_decr_reg(E); break;
+    case MVI_E:  e80_assn_reg(E, lo_data); pc++; break;
+    case RAR:    e80_rrotate_c(A); break;
  
     case RIM:    break; /* not implemented in 8080 */
-    case LXI_H:  emu8080_assn_rp(H, data2, data1); pc += 2; break;
-    case SHLD:
+    case LXI_H:  e80_assn_rp(H, hi_data, lo_data); pc++; pc++; break;
+    case SHLD:   e80_write_mem_rpdata(H, hi_data, lo_data); pc++; pc++; break;
+    case INX_H:  e80_incr_rp(H); break;
+    case INR_H:  e80_incr_reg(H); break;
+    case DCR_H:  e80_decr_reg(H); break;
+    case MVI_H:  e80_assn_reg(H, lo_data); pc++; break;
+    case DAA: {
+      unsigned lsbs = *A & 0x0f;
+      if (e80_get_flag(FLAG_AC) || lsbs > 9) {
+        e80_add(6U);
+      }
+      unsigned msbs = *A >> 4;
+      if (e80_get_flag(FLAG_CY) || msbs > 9) {
+	msbs = (msbs + 6U) << 4;
+      }
+      *A = (*A & 0x0fU) | msbs;
       break;
-    case INX_H:
-      break;
-    case INR_H:
-      break;
-    case DCR_H:
-      break;
-    case MVI_H:
-      break;
-    case DAA:
-      break;
-    case LDHI:
-      break;
+    }
+    case LDHI:   break; /* not implemented in 8080 */
     case DAD_H:
       break;
     case LHLD:
